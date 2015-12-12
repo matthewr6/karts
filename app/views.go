05ad8d7b1a7ab0fb4b41e9a04f7cb5961dedeb49
Firedrake969 <firedrake969@users.noreply.github.com>
2015-12-11 19:40:46 -0700

    remove commented code

--- app/views.go
+++ app/views.go
@@ -8,14 +8,6 @@ import (
     "../framework/views"
 )
 
-// func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-//     fmt.Fprint(w, "Welcome!\n")
-// }
-
-// func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-//     fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-// }
-
 var Index = views.View{
     Get: func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
         views.TemplateRender("test.html", w, r, ps) // repeats... not good
