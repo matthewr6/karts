d23f4e78e66f2f40f3d5ec7c933f760f9375e408
Firedrake969 <firedrake969@users.noreply.github.com>
2015-12-18 17:32:27 -0700

    somehow this wasn't added

--- example/urls.go
+++ example/urls.go
@@ -5,25 +5,6 @@ import (
     "github.com/firedrake969/karts/views"
 )
 
-var Index = views.View{
-    Get: func(c *views.Context) {
-        views.TemplateRender("test.html", c)
-    },
-    Form: views.Form{
-        Fields: map[string]views.Field{
-            "inputfield": {
-                Required: true,
-                Type: "text",
-            },
-            "selection": {
-                Required: true,
-                Type: "multiple",
-            },
-        },
-        SuccessUrl: "/test/someurl",
-    },
-}
-
 var Routes = map[string]views.View {
     "/": Index,
     "/test/:param": Index,
